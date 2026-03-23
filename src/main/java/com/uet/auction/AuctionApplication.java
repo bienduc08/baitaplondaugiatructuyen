9cbd09623737096fc2b4e946e810023e4b1cf0cf
bienduc08 <bienduc08@gmail.com>
2026-03-23 18:53:46 +0700

    Tao cau truc thu muc chuan Layered Architecture

--- src/main/java/com/uet/auction/AuctionApplication.java
+++
@@ -1,7 +0,0 @@
-package com.uet.auction;
-
-public class AuctionApplication {
-    public static void main(String[] args) {
-        System.out.println("Chào mừng bạn đến với hệ thống Đấu giá trực tuyến UET!");
-    }
-}
